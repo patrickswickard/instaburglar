12ae08d913bb9b024fc87ffc6112b3ac729130ba
Patrick Swickard <swickape@hotmail.com>
2023-10-30 11:15:37 -0400

    links added

--- findhttp.py
+++ findhttp.py
@@ -1,21 +1,28 @@
 import re
 
-filename = 'instapost7.txt'
-with open(filename) as fd:
-  lines = fd.read().splitlines()
+for i in range(11,70):
+  filename = 'instapost' + str(i) + '.txt'
+  outfilename = 'allurls' + str(i) + '.txt'
+  print(filename)
+  print(outfilename)
 
-# find and report
-for thisline in lines:
-  branch = re.findall(r"\"http[^\"]*\",\s+\"(?:width|height)\"",thisline)
-  branch = re.findall(r"\"(http[^\"]*)\",\s*\"(?:width|height)\"",thisline)
-  if branch:
-#    print(branch.group(1))
-    for url in branch:
-      newurl = re.sub(r"\\/","/",url)
-      print(newurl)
+  with open(filename) as fd:
+    lines = fd.read().splitlines()
+    thisoutfile = open(outfilename, 'w')
 
+  # find and report
+  for thisline in lines:
+    branch = re.findall(r"\"http[^\"]*\",\s+\"(?:width|height)\"",thisline)
+    branch = re.findall(r"\"(http[^\"]*)\",\s*\"(?:width|height)\"",thisline)
+    if branch:
+  #    print(branch.group(1))
+      for url in branch:
+        newurl = re.sub(r"\\/","/",url) + "\n"
+        print(newurl)
+        thisoutfile.write(newurl)
 
-# find and report
-for thisline in lines:
-  newline = re.sub(r"\W","",thisline)
-#  print(newline)
+  # find and report
+  for thisline in lines:
+    newline = re.sub(r"\W","",thisline)
+  #  print(newline)
+  print(filename)
--- makelinks.py
+++ makelinks.py
@@ -1,14 +1,17 @@
 import re
 
-file = 'allurls7.txt'
-with open(file) as fd:
-  lines = fd.read().splitlines()
-
-count = 0
-for line in lines:
-  hit = re.findall(r"\?stp=dst-jpg_e35&",line)
-  if hit:
-    count += 1
-    print("<a href=\"" + line + "\">LINK " + str(count) + "</a><br>")
+for i in range(11,70):
+  infilename = 'allurls' + str(i) + '.txt'
+  outfilename = 'spit_url' + str(i) + '.html'
+  with open(infilename) as fd:
+    thisoutfile = open(outfilename, 'w')
+    lines = fd.read().splitlines()
+    count = 0
+    for line in lines:
+      hit = re.findall(r"\?stp=dst-jpg_e35&",line)
+      if hit:
+        count += 1
+        print("<a href=\"" + line + "\">LINK " + str(count) + "</a><br>")
+        thisoutfile.write("<a href=\"" + line + "\">LINK " + str(count) + "</a><br>\n")
 
 
