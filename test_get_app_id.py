e126606650c1dfadac0fb1fe90ff244da56075be
Patrick Swickard <swickape@hotmail.com>
2023-11-30 10:14:34 -0500

    cleanup unused code

--- test_get_app_id.py
+++ test_get_app_id.py
@@ -12,6 +12,5 @@ secret = mysecret.Mysecret()
 sessionid = secret.sid
 
 my_user = instatools.Instauser()
-#my_user.get_user_from_web(username)
 my_user_appid = my_user.get_app_id(username)
 print('app id is ' + my_user_appid)
