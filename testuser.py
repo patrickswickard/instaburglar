9d3ef213fb45a45c4891ef92ede21060a8b5a687
Patrick Swickard <swickape@hotmail.com>
2023-11-30 10:25:11 -0500

    cleanup

--- testuser.py
+++ testuser.py
@@ -1,14 +1,6 @@
-import requests
-import re
-import json
-import shutil
-import mysecret
-import time
 import instatools
 
 username = 'bugbobbie'
-secret = mysecret.Mysecret()
-sessionid = secret.sid
 
 my_user = instatools.Instauser()
 my_user.get_user_from_web(username)
