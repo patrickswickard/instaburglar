7ca9d01302af4e8e24756692d0557161c94c7a00
Patrick Swickard <swickape@hotmail.com>
2023-12-21 11:29:45 -0500

    checkin appid test changes

--- test_get_app_id.py
+++ test_get_app_id.py
@@ -5,3 +5,7 @@ username = 'drought_season'
 my_user = instatools.Instauser()
 my_user_appid = my_user.get_app_id(username)
 print('app id is ' + my_user_appid)
+#################
+print('one more time...')
+my_user_appid2 = instatools.Instauser.get_app_id('drought_season')
+print('app id 2 is ' + my_user_appid2)
