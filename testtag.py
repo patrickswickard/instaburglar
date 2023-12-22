0500e78ce1312c233d11ccb802bd4e66f0968d30
Patrick Swickard <swickape@hotmail.com>
2023-12-22 10:09:37 -0500

    testtag pylint fixes

--- testtag.py
+++ testtag.py
@@ -1,12 +1,12 @@
+"""Test functionality of getting all posts a user is tagged in."""
 import instatools
-import json
 
-username = 'corvid_phalanges'
+USERNAME = 'corvid_phalanges'
 
 my_user = instatools.Instauser()
-my_user.get_user_from_web(username)
-my_user_appid = my_user.get_app_id(username)
-my_user.get_user_from_web(username)
+my_user.get_user_from_web(USERNAME)
+my_user_appid = my_user.get_app_id(USERNAME)
+my_user.get_user_from_web(USERNAME)
 response = my_user.get_first_set_tagged()
 all_data_list_tagged = my_user.get_all_data_list_tagged()
 print(all_data_list_tagged)
