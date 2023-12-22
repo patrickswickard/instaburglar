fc5ba698d7d1909a9b112e5de1bf480b39beeff1
Patrick Swickard <swickape@hotmail.com>
2023-12-22 10:49:48 -0500

    generate cloud from alljson pylint fixes

--- generate_word_cloud_from_alljson.py
+++ generate_word_cloud_from_alljson.py
@@ -1,12 +1,15 @@
+"""Generate a wordcloud from a given alljson file"""
 import json
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
-import pandas as pd
 
+INFILENAME = 'babysonfirecafe_all_data_list.json'
 
-json_file = open('babysonfirecafe_all_data_list.json', 'r')
-json_hash = json.load(json_file)
-json_file.close()
+#json_file = open('babysonfirecafe_all_data_list.json', 'r')
+#json_hash = json.load(json_file)
+#json_file.close()
+with open(INFILENAME,'r',encoding = 'utf-8') as json_file:
+  json_hash = json.load(json_file)
 
 post_list = json_hash
 
@@ -29,27 +32,27 @@ for thiscaption in all_caption_list:
   if thiscaption:
     print(thiscaption)
 
-allstring = ' '.join(all_caption_list)
+ALLSTRING = ' '.join(all_caption_list)
 
-tokens = allstring.split()
+tokens = ALLSTRING.split()
+tokens = [x.lower() for x in tokens]
+#for i in range(len(tokens)):
+#  tokens[i] = tokens[i].lower()
 
-for i in range(len(tokens)):
-  tokens[i] = tokens[i].lower()
-
-caption_words = ""
-caption_words += " ".join(tokens)+" "
+CAPTION_WORDS = ""
+CAPTION_WORDS += " ".join(tokens)+" "
 
 stopwords = set(STOPWORDS)
 
 wordcloud = WordCloud(width = 800, height = 800,
                 background_color ='white',
                 stopwords = stopwords,
-                min_font_size = 10).generate(caption_words)
- 
-# plot the WordCloud image                       
+                min_font_size = 10).generate(CAPTION_WORDS)
+
+# plot the WordCloud image
 plt.figure(figsize = (8, 8), facecolor = None)
 plt.imshow(wordcloud)
 plt.axis("off")
 plt.tight_layout(pad = 0)
- 
+
 plt.show()
